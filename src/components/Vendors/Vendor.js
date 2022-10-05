94747c88d60858ef90d2e7f9907508171efaf50f
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-10-05 07:35:19 +0300

    Added form

--- src/components/Vendors/Vendor.js
+++ src/components/Vendors/Vendor.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Vendor = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState(0);
+  const uploadImage = (files) => {
+    console.log(files[0]);
+  };
   return (
-    <div>Vendor</div>
-  )
-}
+    <div>
+      <input type="title" onChange={(e) => setTitle(e.target.value)} />
+      <input
+        type="file"
+        onChange={(e) => {
+          uploadImage(e.target.files);
+        }}
+      />
+      <input
+        type="description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input type="category" onChange={(e) => setCategory(e.target.value)} />
+      <input type="price" onChange={(e) => setPrice(e.target.value)} />
+      <input type="submit" onClick={() => {}} />
+    </div>
+  );
+};
 
-export default Vendor
+export default Vendor;
